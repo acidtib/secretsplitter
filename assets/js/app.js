766939ea4dcab0600df24acc776d7a84d921c62f
Dainel Vera <arubinofaux@gmail.com>
2020-07-23 23:16:57 -0400

    wip

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   init()
 
   $(".split-btns .split-type").on("click", function (event) {
-    event.preventDefault();
+    event.preventDefault()
 
     let type = $(this).data("split")
     let normal = ""
@@ -12,23 +12,29 @@ $(document).ready(function(){
     if (type == "seed") {
       seedSize = $(this).text()
       $(".seed-size").text(": " + seedSize)
+      renderSeedInput(seedSize)
     }
 
     normal = normalName(type)
 
     $(".splitting").text(": " + normal)
 
-    console.log(type)
-    console.log(seedSize)
+    render(type)
+  });
+
+  $(".split-the-secret .btn").on("click", function (event) {
+    event.preventDefault()
+
+    splitSecret("helllo")
   });
 
 });
 
 function init() {
-  console.log("init")
-
   $(".splitting").text(": Mnemonic Seed")
   $(".seed-size").text(": 12")
+  renderSeedInput(12)
+  render('seed')
 }
 
 function normalName(type) {
@@ -45,4 +51,68 @@ function normalName(type) {
   }
 }
 
+function render(type) {
+  $('.split-the-secret').attr("data-secreto", type)
+  
+  switch (type) {
+    case "seed":
+      $('.entry-private-key').hide()
+      $('.entry-text').hide()
 
+      $('.entry-seed').show()
+      break;
+    case "private-key":
+      $('.entry-seed').hide()
+      $('.entry-text').hide()
+
+      renderPrivateKeyInput()
+      break;
+    case "text":
+      $('.entry-private-key').hide()
+      $('.entry-seed').hide()
+
+      renderTextInput()
+      break;
+  }
+}
+
+function renderSeedInput(words) {
+  $('.seed-input-group').empty();
+
+  $.each(new Array(parseInt(words)), function(n) {
+    let num = n + 1
+
+    let temp = `
+      <div class="col-md-4 mb-3">
+        <div class="input-group">
+          <span class="input-group-text">`+ num + `.</span>
+          <input type="text" class="form-control">
+        </div>
+      </div>
+    `
+
+    $('.seed-input-group').append(temp)
+  });
+}
+
+function renderPrivateKeyInput() {
+  $('.entry-private-key').show()
+}
+
+function renderTextInput() {
+  $('.entry-text').show()
+}
+
+function splitSecret(secret) {
+  // convert the text into a hex string
+  var toHex = secrets.str2hex(secret) // => hex string
+
+  // split into 5 shares, with a threshold of 3
+  var parts = secrets.share(toHex, 5, 3)
+
+
+  $('.split-parts').append(parts)
+
+
+  console.log(parts)
+}
